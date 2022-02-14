902a513cac5cfce2b010e1b5565a150fd40ca363
Jorge Totena <jorge.totena@gmail.com>
2022-02-14 18:50:05 -0500

    Desarollando la lógica de la aplicación

--- encrypt.js
+++ encrypt.js
@@ -1,3 +1,37 @@
+var encrypt = document.querySelector("#btn-encriptar")
+var message = document.querySelector("#input-texto")
+var capturedMessage = capturarMensaje(message);
+var comparedMessage = compararMensaje();
+
+encrypt.addEventListener("click", function(event){
+    event.preventDefault();
+    console.log("you have clicked the button")
+        
+
+});
+
+function capturarMensaje(message) {
+    message.addEventListener("input", function(){
+        var input = this.value;
+        console.log(input);       
+              
+     });
+    
+}
+
+
+
+
+ function compararMensaje() {
+     if (message.textContent == "a") {
+         alert("ai")
+
+     }
+
+
+}
+/*
+
 
 /* Reglas de encriptación: 
 "e" es convertido para "enter" 
